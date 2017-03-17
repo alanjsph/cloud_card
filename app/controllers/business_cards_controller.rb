00e42114ab182614b8a7da739cd85f52f760e506
Alan Joseph <alanjsph@live.com>
2017-03-17 20:29:38 +0530

    feature/creation/ apis definition

--- app/controllers/business_cards_controller.rb
+++ app/controllers/business_cards_controller.rb
@@ -7,6 +7,17 @@ class BusinessCardsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @business_cards = @user.business_cards.all
+    @business_cards = @user.business_cards
+    #@business_cards = @user.business_cards.all
+    email = @user.business_cards.pluck(:email).map.with_index{|email, index| {index => email}}.reduce(:merge)
+    render json: @business_cards
+    #render json: {alan: 123}
   end
+
+  def email
+    @user = User.find(params[:id])
+    email = @user.business_cards.pluck(:email).map.with_index{|email, index| {index => email}}.reduce(:merge)
+    render json: {"email" => email}
+  end
+
 end
