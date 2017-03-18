a1be512a373d5cc954cd1fd7d96b46fb5bb43c60
Alan Joseph <alanjsph@live.com>
2017-03-18 12:46:03 +0530

    setting routes done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def receivedcards
     @user = User.find(params[:id])
     connections = UserConnection.where({:user => @user})
-    render json: connections.map { |c| {"card" => c.business_card, "note" => c.notes} }.reduce(:merge)
+    render json: connections.map { |c| {"card" => c.business_card, "note" => c.notes} }.reduce(:merge), status: 200
     #render json: {"sd":"sd"}
   end
 
@@ -16,14 +16,14 @@ class UsersController < ApplicationController
     @business_cards = @user.business_cards
     #@business_cards = @user.business_cards.all
     email = @user.business_cards.pluck(:email).map.with_index{|email, index| {index => email}}.reduce(:merge)
-    render json: email
+    render json: email, status: 200
     #render json: {alan: 123}
   end
 
   def ownedbusinesscards
     @user = User.find(params[:id])
     @business_cards = @user.owned_business_cards
-    render json: @business_cards
+    render json: @business_cards, status: 200
     #render json: {alan: 123}
   end
 
